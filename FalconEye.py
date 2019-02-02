60ff38e8923b028ca5e03b5439b959698ee158f1
Cierra O <cc362x@gmail.com>
2019-02-02 16:06:36 -0500

    Draws rectangles around contours

--- FalconEye.py
+++ FalconEye.py
@@ -5,21 +5,27 @@ import FalconEyeMap
 # Creates a capture from the specified camera or file
 cap = cv2.VideoCapture(FalconEyeMap.TEST_CAM_1)
 
+# Variables for the brightness threshold
+lower_brightness = 200
+upper_brightness = 255
+
 while(1):
     _, frame = cap.read()
 
+    # Converts frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Sets the threshold for what brightness objects of interest must be
-    _, thresh = cv2.threshold(gray, 200, 255, 0)
+    _, thresh = cv2.threshold(gray, lower_brightness, upper_brightness, 0)
     # Finds the contours of everything within the brightness threshold
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     cv2.drawContours(frame, contours, -1, (0,255,0), 3)
 
-    # If there are any contours, finds the center of each
+    # If there are any contours, finds the center of each and draws rectangles around them
     if(len(contours) > 0):
         for i in contours:
             if(cv2.contourArea(i) > 50):
+                # Finds the center of the contour
                 M = cv2.moments(i)
                 cX = int(M['m10'] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
@@ -27,11 +33,19 @@ while(1):
                 # Draws a dot in the center of the contour
                 cv2.circle(frame, (cX,cY), 5, (0,0,255), -1)
 
+                # Draws a rectangle around the contour
+                rect = cv2.minAreaRect(i)
+                box = cv2.boxPoints(rect)
+                box = numpy.int0(box)
+                cv2.drawContours(frame, [box], 0, (0,0,255), 2)
+
     cv2.imshow("frame", frame)
     cv2.imshow("thresh", thresh)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
+
+cap.release()
 
 cv2.destroyAllWindows()
