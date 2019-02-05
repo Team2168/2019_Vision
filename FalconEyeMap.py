ccf48cdc87998c14e229466d2ea665587fe5f396
Ricardo Ortiz <ricortiz13@gmail.com>
2019-02-04 22:34:38 -0500

    Cierra working from Ricardo's laptop, implementing vision filters

--- FalconEyeMap.py
+++ FalconEyeMap.py
@@ -2,3 +2,8 @@ TEST_CAM_1 = 0
 TEST_CAM_2 = 1
 FRONT_CAM_IP = "http://10.21.68.90/mjpg/video.mjpg"
 BACK_CAM_IP = "http://10.21.68.91/mjpg/video.mjpg"
+
+LOWER_BRIGHTNESS = 250
+UPPER_BRIGHTNESS = 255
+
+CONTOUR_SIZE_THRESHOLD = 0.1
