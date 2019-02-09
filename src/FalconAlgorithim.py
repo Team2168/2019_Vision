adfca552f8fea54013b8c885053ca9642b6544a5
Cierra O <cc362x@gmail.com>
2019-02-08 19:45:31 -0500

    Added comments

--- src/FalconAlgorithim.py
+++ src/FalconAlgorithim.py
@@ -10,6 +10,7 @@ rect = None
 roiRect = None
 aspectRatio = None
 
+# Determines if a contour is a vision target
 def contourIsTarget(contour, side):
     if side == "left":
         targetAspectRatio = FalconEyeMap.TARGET_ASPECT_RATIO_LEFT
@@ -22,10 +23,10 @@ def contourIsTarget(contour, side):
     else:
         return False
     
-    # Filters out contours with different aspect ratios than the vision target
+    # Confirms contour has the same aspect ratio as the vision target
     if aspectRatio > (targetAspectRatio - targetAspectRatio*FalconEyeMap.TARGET_ASPECT_RATIO_PERCENTAGE) and aspectRatio < (targetAspectRatio + targetAspectRatio*FalconEyeMap.TARGET_ASPECT_RATIO_PERCENTAGE):
         angle = tempRect[2]
-        # Confirms contour is a left or right vision target
+        # Confirms contour has the same angle as the vision target
         if angle > (targetAngleDegrees + targetAngleDegrees*FalconEyeMap.TARGET_ANGLE_PERCENTAGE) and angle < (targetAngleDegrees - targetAngleDegrees*FalconEyeMap.TARGET_ANGLE_PERCENTAGE):
             return True
         else:
@@ -43,16 +44,16 @@ while 1:
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(grayscale, FalconEyeMap.LOWER_BRIGHTNESS, FalconEyeMap.UPPER_BRIGHTNESS, 0)
 
+    # Stores the number of rows and columns in thresh
     rows = thresh.shape[0]
     cols = thresh.shape[1]
    
     # Finds the contours of everything within the brightness threshold
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # First check: ensures there are at least two contours
     contoursLen = len(contours)
     i = 0
-
+    # First check: ensures there are at least two contours
     if len(contours) >= 2:
         while i < contoursLen:
             currContour = contours[i]
@@ -67,7 +68,9 @@ while 1:
         for contour in contours:
             rect = cv2.minAreaRect(contour)
             aspectRatio = rect[1][0] / rect[1][1]
+            # If the contour is a left vision target, finds the right vision target
             if contourIsTarget(contour, "left"):
+                # Stores the center and dimensions of the contour
                 cx = rect[0][0]
                 cy = rect[0][1]
                 contourWidth = rect[1][1]
@@ -92,30 +95,39 @@ while 1:
                 roi = thresh[lowerRow:upperRow, leftmostCol:rightmostCol]
                 subframe = frame[lowerRow:upperRow, leftmostCol:rightmostCol]
                 cv2.imshow("Roi", roi)
+                # Finds and sorts the contours within the range of interest
                 roiContours, hierarchy = cv2.findContours(roi, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
                 roiContours = sorted(roiContours, key=lambda x: cv2.contourArea(x), reverse=True)
                 if len(roiContours) >= 2:
                     for roiContour in roiContours:
                         roiRect = cv2.minAreaRect(roiContour)
                         aspectRatio = roiRect[1][0] / roiRect[1][1]
+                        # If the contour is a right vision target, finds the distance between the center of both targets and the center of the frame
                         if contourIsTarget(roiContour, "right"):
+                            # Stores the center of the contour
                             cx2 = roiRect[0][0] + leftmostCol
                             cy2 = roiRect[0][1] + lowerRow
+                            # Stores the midpoint between the two vision targets
                             midpointX = int((cx + cx2)/2)
                             midpointY = int((cy + cy2)/2)
+                            # Stores the center of the frame
                             frameCenterX = int((0 + cols)/2)
                             frameCenterY = int((0 + rows)/2)
+                            # Finds the distance and draws a line from the midpoint to the center of the frame
                             distance = abs(int(midpointX - frameCenterX))
                             cv2.line(frame, (midpointX, midpointY), (frameCenterX, frameCenterY), (0,0,255), 3)
                             break
-            
+
+                # Draws the contours onto subframe and then puts them onto frame
                 cv2.drawContours(subframe, roiContours, -1, (0,255,0), 3)
                 frame[lowerRow:upperRow, leftmostCol:rightmostCol] = subframe
                 break
 
+    # Draws a vertical line in the center of the frame before showing the frame
     cv2.line(frame, (int((0 + cols)/2), 0), (int((0 + cols)/2), rows), (0,255,0), 3)
     cv2.imshow("frame", frame)
 
+    # Closes the program if Esc is pressed
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
