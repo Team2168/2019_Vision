e21f660f9aaa77b5e766b94e3cbf08c748701e92
Cierra O <cc362x@gmail.com>
2019-02-11 19:49:03 -0500

    Finds the distance between midpoint of targets and center of frame

--- src/LargestAlgorithm.py
+++ src/LargestAlgorithm.py
@@ -6,6 +6,8 @@ import time
 # Creates a capture from the specified camera or file
 cap = cv2.VideoCapture('../target_samples/field_static.avi')
 
+distance = None
+
 while(1):
     # Slows video to 10 fps
     time.sleep(0.1)
@@ -15,6 +17,10 @@ while(1):
     # Converts frame to grayscale, then to a binary image
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(grayscale, FalconEyeMap.LOWER_BRIGHTNESS, FalconEyeMap.UPPER_BRIGHTNESS, 0)
+
+    # Stores the number of rows and columns in thresh
+    rows = thresh.shape[0]
+    cols = thresh.shape[1]
 
     # Finds the contours within the brightness threshold
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -36,6 +42,25 @@ while(1):
         secondLargestBox = np.int0(secondLargestBox)
         cv2.drawContours(frame, [largestBox], 0, (0,0,255), 2)
         cv2.drawContours(frame, [secondLargestBox], 0, (0,0,255), 2)
+
+        # Stores centers of targets and center of frame
+        largestRectCX = largestRect[0][0]
+        largestRectCY = largestRect[0][1]
+        secondLargestRectCX = secondLargestRect[0][0]
+        secondLargestRectCY = secondLargestRect[0][1]
+        frameCenterX = int((0 + cols) / 2)
+        frameCenterY = int((0 + rows) / 2)
+
+        # Calculates midpoint of targets and distance between midpoint and center of frame
+        midpointX = int((largestRectCX + secondLargestRectCX) / 2)
+        midpointY = int((largestRectCY + secondLargestRectCY) / 2)
+        distance = abs(int(frameCenterX - midpointX))
+
+        # Draws a line from midpoint to center of frame
+        cv2.line(frame, (midpointX, midpointY), (frameCenterX, frameCenterY), (0,0,255), 3)
+
+    # Draws a vertical line in the center of frame
+    cv2.line(frame, (frameCenterX, 0), (frameCenterX, rows), (0,255,0), 3)
 
     # Displays the frame and thresh
     cv2.imshow("frame", frame)
