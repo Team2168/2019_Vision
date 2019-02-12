bd7c4e8265a23b53206d597017d539cb3a651aff
Cierra O <cc362x@gmail.com>
2019-02-12 14:04:34 -0500

    Finds the percent error

--- src/ErrorAlgorithm.py
+++ src/ErrorAlgorithm.py
@@ -18,8 +18,29 @@ while(1):
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(grayscale, FalconEyeMap.LOWER_BRIGHTNESS, FalconEyeMap.UPPER_BRIGHTNESS, 0)
 
-    # Finds the contours withinh the brightness threshold
+    # Finds the contours within the brightness threshold
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        rect = cv2.minAreaRect(contour)
+        aspectRatio = rect[1][0] / rect[1][1]
+        angle = rect[2]
+
+        # Finds the percent error for aspect ratio and angle
+        rightAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT) / FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT)
+        leftAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_LEFT) / FalconEyeMap.TARGET_ASPECT_RATIO_LEFT)
+        if rightAspectRatioError <= leftAspectRatioError:
+            aspectRatioError = rightAspectRatioError
+        else:
+            aspectRatioError = leftAspectRatioError
+        rightAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT) / FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT)
+        leftAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT) / FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT)
+        if rightAngleError <= leftAngleError:
+            angleError = rightAngleError
+        else:
+            angleError = leftAngleError
+        
+        error = aspectRatioError + angleError
 
     # Displays frame and thresh
     cv2.imshow("frame", frame)
