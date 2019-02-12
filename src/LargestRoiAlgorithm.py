1cd922ef8b2ea0b72442d3f0a20758bde65950da
Cierra O <cc362x@gmail.com>
2019-02-11 21:40:33 -0500

    Finds distance between midpoint of targets and center of frame

--- src/LargestRoiAlgorithm.py
+++ src/LargestRoiAlgorithm.py
@@ -76,9 +76,21 @@ while(1):
             cv2.drawContours(subframe, [secondLargestBox], 0, (0,255,0), 2)
             frame[lowerRow:upperRow, leftmostCol:rightmostCol] = subframe
 
-            # Stores center of second-largest target
+            # Stores center of second-largest target and frame
             secondLargestRectCX = secondLargestRect[0][0] + leftmostCol
             secondLargestRectCY = secondLargestRect[0][1] + lowerRow
+            frameCenterX = int((0 + cols) / 2)
+
+            # Calculates midpoint of targets and distance between midpoint and center of frame
+            midpointX = int((largestRectCX + secondLargestRectCX) / 2)
+            midpointY = int((largestRectCY + secondLargestRectCY) / 2)
+            distance = abs(int(frameCenterX - midpointX))
+
+            # Draws a line from midpoint to center of frame
+            cv2.line(frame, (midpointX, midpointY), (frameCenterX, midpointY), (0,0,255), 3)
+
+    # Draws a vertical line in the center of frame
+    cv2.line(frame, (frameCenterX, 0), (frameCenterX, rows), (0,255,0), 3)
 
     # Displays frame and thresh
     cv2.imshow("frame", frame)
