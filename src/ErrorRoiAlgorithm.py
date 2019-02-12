d575ad2cb5673c350731d5d73b022d332cb38166
Cierra O <cc362x@gmail.com>
2019-02-12 17:49:17 -0500

    Finds distance between midpoint of targets and center of frame

--- src/ErrorRoiAlgorithm.py
+++ src/ErrorRoiAlgorithm.py
@@ -6,6 +6,46 @@ import time
 # Creates a capture from the specified camera or file
 cap = cv2.VideoCapture('../target_samples/field_panel_2_rocket.avi')
 
+distance = None
+
+def findBestTarget(contoursList):
+    lowestError = None
+    bestTarget = None
+    bestTargetIsRight = None
+    for contour in contoursList:
+        rect = cv2.minAreaRect(contour)
+        aspectRatio = rect[1][0] / rect[1][1]
+        angle = rect[2]
+
+        # Finds the percent error for aspect ratio and angle
+        rightAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT) / FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT)
+        leftAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_LEFT) / FalconEyeMap.TARGET_ASPECT_RATIO_LEFT)
+        if rightAspectRatioError <= leftAspectRatioError:
+            aspectRatioError = rightAspectRatioError
+        else:
+            aspectRatioError = leftAspectRatioError
+        rightAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT) / FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT)
+        leftAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT) / FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT)
+        if rightAngleError <= leftAngleError:
+            angleError = rightAngleError
+        else:
+            angleError = leftAngleError
+
+        error = aspectRatioError + angleError
+
+        # Determines if the contour is a right or left target
+        if rightAspectRatioError + rightAngleError <= leftAspectRatioError + leftAngleError:
+            isRight = True
+        else:
+            isRight = False
+
+        # If there is no lowest error, then this contour has the lowest error and is the new best target
+        if lowestError == None or error <= lowestError:
+            lowestError = error
+            bestTarget = contour
+            bestTargetIsRight = isRight
+    return bestTarget, bestTargetIsRight
+
 while(1):
     # Slows video to 10 fps
     time.sleep(0.1)
@@ -35,41 +75,7 @@ while(1):
         i+=1
 
     if len(contours) >= 2:
-        lowestError = None
-        bestTarget = None
-        bestTargetIsRight = None
-        for contour in contours:
-            rect = cv2.minAreaRect(contour)
-            aspectRatio = rect[1][0] / rect[1][1]
-            angle = rect[2]
-
-            # Finds the percent error for aspect ratio and angle
-            rightAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT) / FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT)
-            leftAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_LEFT) / FalconEyeMap.TARGET_ASPECT_RATIO_LEFT)
-            if rightAspectRatioError <= leftAspectRatioError:
-                aspectRatioError = rightAspectRatioError
-            else:
-                aspectRatioError = leftAspectRatioError
-            rightAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT) / FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT)
-            leftAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT) / FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT)
-            if rightAngleError <= leftAngleError:
-                angleError = rightAngleError
-            else:
-                angleError = leftAngleError
-
-            error = aspectRatioError + angleError
-
-            # Determines if the contour is a right or left target
-            if rightAspectRatioError + rightAngleError <= leftAspectRatioError + leftAngleError:
-                isRight = True
-            else:
-                isRight = False
-
-            # If there is no lowest error, then this contour has the lowest error and is the new best target
-            if lowestError == None or error <= lowestError:
-                lowestError = error
-                bestTarget = contour
-                bestTargetIsRight = isRight
+        bestTarget, bestTargetIsRight = findBestTarget(contours)
 
         # Draws a rectangle around the best target
         bestRect = cv2.minAreaRect(bestTarget)
@@ -84,7 +90,7 @@ while(1):
             bestRectWidth = bestRect[1][0]
             bestRectHeight = bestRect[1][1]
 
-            rightmostCol = int(bestRectCX + bestRectHeight)
+            rightmostCol = int(bestRectCX - bestRectHeight)
             leftmostCol = int(bestRectCX - 3*bestRectHeight)
             upperRow = int(bestRectCY + 2*bestRectHeight)
             lowerRow = int(bestRectCY - 2*bestRectHeight)
@@ -93,7 +99,7 @@ while(1):
             bestRectHeight = bestRect[1][0]
 
             rightmostCol = int(bestRectCX + 3*bestRectHeight)
-            leftmostCol = int(bestRectCX - bestRectHeight)
+            leftmostCol = int(bestRectCX + bestRectHeight)
             upperRow = int(bestRectCY + 2*bestRectHeight)
             lowerRow = int(bestRectCY - 2*bestRectHeight)
 
@@ -109,6 +115,46 @@ while(1):
 
         roi = thresh[lowerRow:upperRow, leftmostCol:rightmostCol]
         subframe = frame[lowerRow:upperRow, leftmostCol:rightmostCol]
+
+        # Finds the contours in the ROI
+        contours, hierarchy = cv2.findContours(roi, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Filters out contours with small areas
+        i = 0
+        contoursLen = len(contours)
+        while i < contoursLen:
+            currContour = contours[i]
+            if cv2.contourArea(currContour) < FalconEyeMap.CONTOUR_SIZE_THRESHOLD:
+                del contours[i]
+                i-=1
+                contoursLen-=1
+            i+=1
+
+        if len(contours) >= 1:
+            secondBestTarget, _ = findBestTarget(contours)
+
+            # Draws a rectangle around the second-best target
+            secondBestRect = cv2.minAreaRect(secondBestTarget)
+            secondBestBox = cv2.boxPoints(secondBestRect)
+            secondBestBox = np.int0(secondBestBox)
+            cv2.drawContours(subframe, [secondBestBox], 0, (0,0,255), 2)
+            frame[lowerRow:upperRow, leftmostCol:rightmostCol] = subframe
+
+            # Stores center of second-best target and frame
+            secondBestRectCX = secondBestRect[0][0] + leftmostCol
+            secondBestRectCY = secondBestRect[0][1] + lowerRow
+            frameCenterX = int(cols / 2)
+
+            # Calculates the midpoint of targets and distance between midpoint and center of frame
+            midpointX = int((bestRectCX + secondBestRectCX) / 2)
+            midpointY = int((bestRectCY + secondBestRectCY) / 2)
+            distance = abs(int(frameCenterX - midpointX))
+
+            # Draws a line from midpoint to center of frame
+            cv2.line(frame, (midpointX, midpointY), (frameCenterX, midpointY), (0,0,255), 3)
+
+    # Draws a vertical line in the center of frame
+    cv2.line(frame, (frameCenterX, 0), (frameCenterX, rows), (0,255,0), 3)
 
     # Displays frame, thresh, and roi
     cv2.imshow("frame", frame)
