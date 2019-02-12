ffb58900bc35331878e044a85902f2619972b637
Cierra O <cc362x@gmail.com>
2019-02-12 16:00:13 -0500

    Displays contours within the brightness threshold

---
+++ src/ErrorRoiAlgorithm.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import FalconEyeMap
+import time
+
+# Creates a capture from the specified camera or file
+cap = cv2.VideoCapture('../target_samples/field_panel_2_rocket.avi')
+
+while(1):
+    # Slows video to 10 fps
+    time.sleep(0.1)
+
+    _, frame = cap.read()
+
+    # Converts frame to grayscale, then to a binary image
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(grayscale, FalconEyeMap.LOWER_BRIGHTNESS, FalconEyeMap.UPPER_BRIGHTNESS, 0)
+
+    # Finds the contours within the brightness threshold
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Displays frame and thresh
+    cv2.imshow("frame", frame)
+    cv2.imshow("thresh", thresh)
+
+    # Closes the program if Esc is pressed
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
