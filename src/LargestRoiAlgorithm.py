45b00654a90f2cc35937615998485107841a386d
Cierra O <cc362x@gmail.com>
2019-02-11 20:03:41 -0500

    Added brightness threshold

---
+++ src/LargestRoiAlgorithm.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import FalconEyeMap
+import time
+
+# Creates a capture from the specified camera or file
+cap = cv2.VideoCapture('../target_samples/field_static.avi')
+
+distance = None
+
+while(1):
+    # Slows video to 10 fps
+    time.sleep(0.1)
+
+    _, frame = cap.read()
+
+    # Converts frame to grayscale, then to a binary image
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(grayscale, FalconEyeMap.LOWER_BRIGHTNESS, FalconEyeMap.UPPER_BRIGHTNESS, 0)
+
+    # Displays frame and thresh
+    cv2.imshow("frame", frame)
+    cv2.imshow("thresh", thresh)
+
+    # Closes the program if Esc is pressed
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
