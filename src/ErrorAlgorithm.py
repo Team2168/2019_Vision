35a26d88cf121f1b6207e456d43431aada0b08cf
Cierra O <cc362x@gmail.com>
2019-02-12 14:28:07 -0500

    Finds the contour with the lowest percent error

--- src/ErrorAlgorithm.py
+++ src/ErrorAlgorithm.py
@@ -21,26 +21,44 @@ while(1):
     # Finds the contours within the brightness threshold
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    for contour in contours:
-        rect = cv2.minAreaRect(contour)
-        aspectRatio = rect[1][0] / rect[1][1]
-        angle = rect[2]
+    if len(contours) >= 2:
+        lowestError = None
+        bestTarget = None
+        for contour in contours:
+            rect = cv2.minAreaRect(contour)
+            aspectRatio = rect[1][0] / rect[1][1]
+            angle = rect[2]
 
-        # Finds the percent error for aspect ratio and angle
-        rightAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT) / FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT)
-        leftAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_LEFT) / FalconEyeMap.TARGET_ASPECT_RATIO_LEFT)
-        if rightAspectRatioError <= leftAspectRatioError:
-            aspectRatioError = rightAspectRatioError
-        else:
-            aspectRatioError = leftAspectRatioError
-        rightAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT) / FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT)
-        leftAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT) / FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT)
-        if rightAngleError <= leftAngleError:
-            angleError = rightAngleError
-        else:
-            angleError = leftAngleError
-        
-        error = aspectRatioError + angleError
+            # Finds the percent error for aspect ratio and angle
+            rightAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT) / FalconEyeMap.TARGET_ASPECT_RATIO_RIGHT)
+            leftAspectRatioError = abs((aspectRatio - FalconEyeMap.TARGET_ASPECT_RATIO_LEFT) / FalconEyeMap.TARGET_ASPECT_RATIO_LEFT)
+            if rightAspectRatioError <= leftAspectRatioError:
+                aspectRatioError = rightAspectRatioError
+            else:
+                aspectRatioError = leftAspectRatioError
+            rightAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT) / FalconEyeMap.TARGET_ANGLE_DEGREES_RIGHT)
+            leftAngleError = abs((angle - FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT) / FalconEyeMap.TARGET_ANGLE_DEGREES_LEFT)
+            if rightAngleError <= leftAngleError:
+                angleError = rightAngleError
+            else:
+                angleError = leftAngleError
+            
+            error = aspectRatioError + angleError
+
+            # If there is no lowest error, then this contour has the lowest error and is the new best target
+            if lowestError == None:
+                lowestError = error
+                bestTarget = contour
+            # Otherwise, if this contour's error is lower than lowestError, it is the new best target
+            elif error <= lowestError:
+                lowestError = error
+                bestTarget = contour
+
+        # Draws a rectangle around the best target
+        bestRect = cv2.minAreaRect(bestTarget)
+        bestBox = cv2.boxPoints(bestRect)
+        bestBox = np.int0(bestBox)
+        cv2.drawContours(frame, [bestBox], 0, (0,0,255), 2)
 
     # Displays frame and thresh
     cv2.imshow("frame", frame)
