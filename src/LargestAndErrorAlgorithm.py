0cfa9c6dd8ffe2a5655badfd379d9cfa5958718e
Cierra O <cc362x@gmail.com>
2019-02-12 19:10:25 -0500

    Finds two best targets

--- src/LargestAndErrorAlgorithm.py
+++ src/LargestAndErrorAlgorithm.py
@@ -21,6 +21,17 @@ while(1):
     # Finds the contours within the brightness threshold
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Filters out contours with small areas
+    i = 0
+    contoursLen = len(contours)
+    while i < contoursLen:
+        currContour = contours[i]
+        if cv2.contourArea(currContour) < FalconEyeMap.CONTOUR_SIZE_THRESHOLD:
+            del contours[i]
+            i-=1
+            contoursLen-=1
+        i+=1
+
     if len(contours) >= 2:
         # Sorts contours from largest to smallest
         sortedContours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
@@ -32,6 +43,10 @@ while(1):
         else:
             largestContours = sortedContours
 
+        lowestError = None
+        secondLowestError = None
+        bestTarget = None
+        secondBestTarget = None
         for contour in largestContours:
             rect = cv2.minAreaRect(contour)
             aspectRatio = rect[1][0] / rect[1][1]
@@ -52,6 +67,45 @@ while(1):
                 angleError = leftAngleError
 
             error = aspectRatioError + angleError
+
+            # If there is no lowest error, then this contour has the lowest error and is the new best target
+            if lowestError == None:
+                lowestError = error
+                bestTarget = contour
+            # Otherwise, if there is no second-lowest error, find one
+            elif secondLowestError == None:
+                # If contour has the new lowest error, contour is the new best target and the previous best is now the second-best
+                if error < lowestError:
+                    secondLowestError = lowestError
+                    secondBestTarget = bestTarget
+                    lowestError = error
+                    bestTarget = contour
+                # If contour doesn't have the new lowest error, it has the second-lowest and is the new second-best contour
+                else:
+                    secondLowestError = error
+                    secondBestTarget = contour
+            # Otherwise, if this contour's error is lower than lowestError, it is the new best target
+            elif error <= lowestError:
+                secondLowestError = lowestError
+                secondBestTarget = bestTarget
+                lowestError = error
+                bestTarget = contour
+            # Otherwise, if this contour's error is lower than secondBestError, it is the new second-best target
+            elif error <= secondLowestError:
+                secondLowestError = error
+                secondBestTarget = contour
+
+        # Draws a rectangle around the best target
+        bestRect = cv2.minAreaRect(bestTarget)
+        bestBox = cv2.boxPoints(bestRect)
+        bestBox = np.int0(bestBox)
+        cv2.drawContours(frame, [bestBox], 0, (0,0,255), 2)
+
+        # Draws a rectangle around the second-best target
+        secondBestRect = cv2.minAreaRect(secondBestTarget)
+        secondBestBox = cv2.boxPoints(secondBestRect)
+        secondBestBox = np.int0(secondBestBox)
+        cv2.drawContours(frame, [secondBestBox], 0, (0,0,255), 2)
 
     # Displays the frame and thresh
     cv2.imshow("frame", frame)
